c2e6696a1689f87b9505faeecf2df2d52550eeff
yumizu-da <yuuki-mizuta@westjr.co.jp>
2024-08-31 00:10:21 +0000

    上田君と長田君のベストスコアをアンサンブル

---
+++ exp/008/src/seed.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+import numpy as np
+import torch
+
+
+def seed_everything(seed: int):
+    """seed値を固定する関数
+
+    Args:
+        seed (int): seed値
+    """
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.use_deterministic_algorithms = True
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
